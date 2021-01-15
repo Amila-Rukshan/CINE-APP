9487cd02e97ba26f6d223306f3687a77dbec1c50
Amila Rukshan Senadheera <amilarukshansenadheera@Amilas-MacBook-Pro.local>
2021-01-15 23:27:06 +0530

    top loading bar added

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,14 @@ import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import Layout from '../components/layout'
 
+import Router from 'next/router';
+import NProgress from 'nprogress'; //nprogress module
+import 'nprogress/nprogress.css'; //styles of nprogress
+//Binding events. 
+Router.events.on('routeChangeStart', () => NProgress.start()); 
+Router.events.on('routeChangeComplete', () => NProgress.done()); 
+Router.events.on('routeChangeError', () => NProgress.done());
+
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
     return (
